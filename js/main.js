cab7dfa6fa286e7f84427ccc2be28969a137b6fb
camelion <jiawei243@gmail.com>
2018-02-11 23:24:40 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ jQuery(document).ready(function($){
 	var month = currentDate.getMonth() + 1
 	var year = currentDate.getFullYear()
 	var date_string = year + ' 年 ' + month + ' 月 ' + day + ' 日'
-	day_count = dateDiffInDays(new Date("1/29/2016"), new Date());
+	day_count = dateDiffInDays(new Date("5/13/2016"), new Date());
 	document.getElementById('days').innerHTML = date_string + '，' + "我们已经在一起 " + day_count + " 天了"
 
 	//on scolling, show/animate timeline blocks when enter the viewport
